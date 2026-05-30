2a71baa97d4ff992b06a4c3d285e68f1a50e86b1
niken2303 <u2302630@gmail.com>
2026-05-30 07:40:48 +0700

    Remove unused Streamlit code for periodic table

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,29 +23,3 @@ st.markdown("""
 
 Gunakan menu di sidebar untuk memulai.
 """)
-import streamlit as st
-
-st.title("📚 Materi Tabel Periodik")
-
-st.markdown("""
-## Pengertian
-
-Tabel periodik adalah susunan unsur-unsur kimia berdasarkan nomor atom dan sifat periodiknya.
-
-## Golongan
-
-- Golongan IA : Logam Alkali
-- Golongan IIA : Alkali Tanah
-- Halogen
-- Gas Mulia
-
-## Periode
-
-Terdapat 7 periode utama.
-
-## Wujud Unsur
-
-- Padat
-- Cair
-- Gas
-""")
