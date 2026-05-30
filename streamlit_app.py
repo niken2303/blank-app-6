7b790678988a5873f1db9ad8c88f3026347e06f6
niken2303 <u2302630@gmail.com>
2026-05-30 08:08:18 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,4 +3,11 @@ import streamlit as st
 st.title("Aplikasi Pembelajaran Tabel Periodik")
 
 st.write("Selamat datang")
+import streamlit as st
+import pandas as pd
 
+st.title("Tabel Periodik")
+
+df = pd.read_csv("data/elements.csv")
+
+st.dataframe(df)
